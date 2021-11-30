53ea3441cc3d75495736c44d838826f45e4dce8a
movie0world <63150363+movie0world@users.noreply.github.com>
2021-11-30 14:34:32 +0500

    comit

--- Backend/route/Website/website.js
+++ Backend/route/Website/website.js
@@ -3,16 +3,41 @@ const mongoose = require("mongoose");
 const website = mongoose.model("Website");
 const { v4 } = require("uuid");
 
+const auth = require("../../Middleware/verifyauth");
+
 const route = express.Router();
 
-route.post("/", async (req, res) => {
-  const web = await website.create({
-    ...req.body,
-    webid: v4(),
-    user: req.user.user_id,
+route.post("/", auth, async (req, res) => {
+  const webfound = await website.findOne({
+    user: mongoose.Types.ObjectId(req.user.user_id),
   });
-  res.send({ webid: web.webid, website: web.domain });
+  if (webfound) {
+    const web = await website.findByIdAndUpdate({
+      ...req.body,
+      webid: v4(),
+      user: req.user.user_id,
+    });
+    res.send({ webid: web.webid, website: web.domain });
+  } else {
+    const web = await website.create({
+      ...req.body,
+      webid: v4(),
+      user: req.user.user_id,
+    });
+    res.send({ webid: web.webid, website: web.domain });
+  }
 });
+
+route.get("/", auth, async (req, res) => {
+  const web = await website.findOne({
+    user: mongoose.Types.ObjectId(req.user.user_id),
+  });
+  if (web) {
+    return res.json(web);
+  }
+  res.json(null);
+});
+
 route.get("/inegration/:webid", async (req, res) => {
   console.log("website staus called how many time");
   await website.findOneAndUpdate(
--- jvsea/src/Component/BrandProfileComponent.js
+++ jvsea/src/Component/BrandProfileComponent.js
@@ -26,6 +26,21 @@ import SideBar from "../UI/SideBar";
 import Spacer from "../UI/Spacer";
 import MyButton from "../UI/MyButton";
 import ApiCall from "../BackendCall";
+import * as Yup from "yup";
+
+const websitvalidation = Yup.object({
+  brand: Yup.string().required(),
+  description: Yup.string().required(),
+  domain: Yup.string().required().url(),
+  category: Yup.string().required(),
+  commission: Yup.number().required(),
+  password: Yup.string()
+    .matches(
+      /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/,
+      "Minimum eight characters, at least one uppercase letter, one lowercase letter and one number"
+    )
+    .required(),
+});
 
 export default function BrandProfileComponent() {
   const formik = useFormik({
@@ -36,7 +51,7 @@ export default function BrandProfileComponent() {
       category: "10",
       domain: "",
     },
-
+    validationSchema: websitvalidation,
     onSubmit: (values) => {
       ApiCall.post("/website", values).then((result) => {
         setwebid(result.data.webid);
@@ -54,8 +69,8 @@ export default function BrandProfileComponent() {
       <div style={{ fontWeight: "bold", fontSize: "25px" }}>Brand Detail</div>
       <Border space="5" />
       <Spacer space="10" />
-      <div>
-        <div style={{ display: "flex" }}>
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <div style={{ display: "flex", flexDirection: "column" }}>
           <TextField
             fullWidth
             style={{ marginRight: "12px" }}
@@ -67,6 +82,12 @@ export default function BrandProfileComponent() {
             onBlur={formik.handleBlur}
             value={formik.values.brand}
           />
+          {formik.touched.brand && formik.errors.brand ? (
+            <div style={{ color: "#B00020" }}>{formik.errors.brand}</div>
+          ) : null}
+        </div>
+
+        <div>
           <FormControl
             variant="outlined"
             fullWidth
@@ -84,12 +105,13 @@ export default function BrandProfileComponent() {
               onBlur={formik.handleBlur}
               label="Category"
             >
-              <MenuItem value={10}>Select the Category</MenuItem>
-              <MenuItem value={20}>Technology</MenuItem>
-              <MenuItem value={30}>Health</MenuItem>
-              <MenuItem value={40}>News</MenuItem>
+              <MenuItem value={10}>Technology</MenuItem>
+              <MenuItem value={20}>Health</MenuItem>
+              <MenuItem value={30}>News</MenuItem>
             </Select>
           </FormControl>
+        </div>
+        <div style={{ display: "flex", flexDirection: "column" }}>
           <TextField
             fullWidth
             style={{ marginRight: "12px" }}
@@ -102,6 +124,11 @@ export default function BrandProfileComponent() {
             onBlur={formik.handleBlur}
             value={formik.values.domain}
           />
+          {formik.touched.domain && formik.errors.domain ? (
+            <div style={{ color: "#B00020" }}>{formik.errors.domain}</div>
+          ) : null}
+        </div>
+        <div style={{ display: "flex", flexDirection: "column" }}>
           <TextField
             style={{ marginRight: "12px" }}
             fullWidth
@@ -115,23 +142,34 @@ export default function BrandProfileComponent() {
             onBlur={formik.handleBlur}
             value={formik.values.commission}
           />
-          <TextField
-            style={{ marginRight: "12px" }}
-            fullWidth
-            id="standard-basic"
-            label="Description"
-            variant="outlined"
-            name="description"
-            placeholder="Enter the Description"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.description}
-          />
+          {formik.touched.commission && formik.errors.commission ? (
+            <div style={{ color: "#B00020" }}>{formik.errors.commission}</div>
+          ) : null}
         </div>
-        <Spacer space="10" />
-        <div style={{ display: "flex", justifyContent: "flex-end" }}>
-          <MyButton onPress={formik.submitForm}>Submit</MyButton>
-        </div>
+      </div>
+      <Spacer space="15" />
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <TextField
+          style={{ marginRight: "12px" }}
+          fullWidth
+          id="standard-basic"
+          label="Description"
+          variant="outlined"
+          name="description"
+          type="textarea"
+          placeholder="Enter the Description"
+          inputProps={{ type: "textarea", row: 10 }}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.description}
+        />
+        {formik.touched.description && formik.errors.description ? (
+          <div style={{ color: "#B00020" }}>{formik.errors.description}</div>
+        ) : null}
+      </div>
+      <Spacer space="10" />
+      <div style={{ display: "flex", justifyContent: "flex-end" }}>
+        <MyButton onPress={formik.submitForm}>Submit</MyButton>
       </div>
       <Spacer space="10" /> <Spacer space="10" />
       <Dialog
